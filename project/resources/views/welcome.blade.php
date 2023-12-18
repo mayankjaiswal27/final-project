58c288cb2d14d2ec0b6926ad9a13dfe7eb3df81b
mayankjaiswal27 <mayankjaiswal0104@gmail.com>
2023-12-18 15:52:08 +0530

    Dashboard

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -220,7 +220,7 @@
                             <button class="button"><a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a></button>
                         @endif
                         </div>
-                        <div class="text-wrapper-4">PROJECT NAME</div>
+                        <div class="text-wrapper-4">Project Manager</div>
                     </div>
                 </div>
             
